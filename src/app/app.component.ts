beaf0d8922347f5ab920aad84a7424c4cfa4a6e3
eslavi <jonathan.medina@jalisco.gob.mx>
2023-04-24 14:46:38 -0600

    Heroes and Counter Modules created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases-angular';
+  // title = 'bases-angular';
+  public title: string = 'Mi primera app de angular';
+  
 }
